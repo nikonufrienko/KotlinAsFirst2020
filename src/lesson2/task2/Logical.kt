01e89be4ca386944c50c7ea332cb2aed0e66234c
nikolay <nikonufrienko@gmail.com>
2020-09-10 22:19:24 +0300

    ---

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -41,7 +41,7 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
  */
 fun daysInMonth(month: Int, year: Int): Int = when {
     month in arrayOf(1, 3, 5, 7, 8, 10, 12) -> 31
-    month == 2 && (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0) -> 29
+    month == 2 && ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) -> 29
     month in arrayOf(4, 6, 9, 11) -> 30
     else -> 28
 }
