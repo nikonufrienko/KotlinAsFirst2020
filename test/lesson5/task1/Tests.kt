64e663cd47bbfc43e558dbc4b6a975ffdfa0d2ec
Nikolay <nikonufrienko@gmail.com>
2020-10-09 23:37:30 +0300

    ---

--- test/lesson5/task1/Tests.kt
+++ test/lesson5/task1/Tests.kt
@@ -335,12 +335,5 @@ class Tests {
                 450
             )
         )
-        assertEquals(
-            setOf<String>("0","1"),
-            bagPacking(
-                mapOf("0" to (1 to 1), "1" to (1 to 1)),
-                2
-            )
-        )
     }
 }
